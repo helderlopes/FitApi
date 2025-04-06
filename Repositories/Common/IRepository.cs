b7dedc042b857090925fab6d791c5fe86246cf5f
helderlopes <helder.giro.lopes@gmail.com>
2025-04-06 19:33:21 -0300

    added dto

--- Repositories/Common/IRepository.cs
+++ Repositories/Common/IRepository.cs
@@ -2,7 +2,7 @@ public interface IRepository<T> where T : class
 {
     Task<T> GetByIdAsync(Guid id);
     Task<IEnumerable<T>> GetAllAsync();
-    Task AddAsync(T entity);
+    Task CreateAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(Guid id);
 }
